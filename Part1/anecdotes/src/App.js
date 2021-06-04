fc5d1f218c8ad0c903aa0e1295596c96950804fa
Aki78 <kmorooka78@gmail.com>
2021-06-05 01:48:25 +0300

    done anecdotes

--- Part1/anecdotes/src/App.js
+++ Part1/anecdotes/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import React, { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients",
+  ];
+  const myRandomInteger = (minVal, maxVal) => {
+    return Math.floor(Math.random() * (maxVal - minVal) + minVal);
+  };
+  const [selected, setSelected] = useState(0);
+  const [upvotes, setUpvote] = useState([0, 0, 0, 0, 0, 0, 0]);
+  console.log(myRandomInteger(0, anecdotes.length));
+  const sumList = (myList) => {
+    return myList.reduce((a, b) => a + b, 0);
+  };
+
+  const upvote = () => {
+    const newUpvotes = [...upvotes];
+    newUpvotes[selected] += 1;
+    setUpvote(newUpvotes);
+    console.log(newUpvotes[0]);
+  };
+  const selectAnec = () => {
+    setSelected(myRandomInteger(0, anecdotes.length));
+  };
+  console.log(upvotes);
+  const printUpvoteNumber = () => {
+    console.log(sumList(upvotes))
+    if (sumList(upvotes) < 1) return ;
+    else return  <div><h3>Most upvotes:</h3> {anecdotes[upvotes.indexOf(Math.max(...upvotes))]}</div>
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1> Anecdote of the day</h1>
+      {anecdotes[selected]} has {upvotes[selected]} votes
+
+      <div>
+        <button onClick={() => selectAnec()}> next anecdotes</button>
+        <button onClick={() => upvote()}> upvote </button>
+      </div>
+      {printUpvoteNumber()}
     </div>
   );
-}
+};
 
 export default App;
