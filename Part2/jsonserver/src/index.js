58846719acba8edab5befb45184920a35fe88251
Aki78 <kmorooka78@gmail.com>
2021-06-08 04:52:18 +0300

    before json server on phonebook

---
+++ Part2/jsonserver/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3002/notes')
+console.log(promise)
+
+const promise2 = axios.get('http://localhost:3002/foobar')
+console.log(promise2)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
