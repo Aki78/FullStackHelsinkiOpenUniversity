d620c850def30b0746e3fa97023d07e4eac8fb79
Aki78 <kmorooka78@gmail.com>
2021-08-01 22:44:33 +0300

    done exercise 4.16

--- Part4/Exercise1/controllers/users.js
+++ Part4/Exercise1/controllers/users.js
@@ -10,15 +10,26 @@ userRouter.get("/", (request, response) => {
 
 userRouter.post("/", async (request, response) => {
   const body = request.body;
-
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(body.password, saltRounds);
+  const usenameExists = await User.find({ username: body.username });
+
+  if (usenameExists) {
+    return response.status(409).json({
+      error: "username already exists.",
+    });
+  }
+
+  if (body.password < 4)
+    return response.status(403).json({
+      error: "password too short.",
+    });
 
   const user = new User({
     username: body.username,
     name: body.name,
+    passwordHash: passwordHash,
     id: body.name,
-    passwordHash,
   });
 
   const savedUser = await user.save();
--- Part4/Exercise1/models/user.js
+++ Part4/Exercise1/models/user.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  name: String,
+  username: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
 });
 
 userSchema.set("toJSON", {
